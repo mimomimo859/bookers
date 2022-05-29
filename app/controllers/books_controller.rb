3a9ab0816cab3c96e6887d46e426e06e8d0ca486
EC2 Default User <ec2-user@ip-172-31-19-153.ec2.internal>
2022-05-29 13:26:38 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,69 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def index
+    @books = Book.all
   end
+
+  def create
+    book = Book.new(book_params)
+
+   if book.save
+
+    flash[:notice] = "Book was successfully created."
+
+    redirect_to book_path(book.id)
+
+   else
+
+     render "new"
+
+   end
+
+  end
+
+  def destroy
+
+    book = Book.find(params[:id])
+
+   if book.destroy
+
+    redirect_to '/books'
+
+    flash[:alert] = "Book was successfully destroyed."
+
+   else
+
+     redirect_to book_index_path
+
+   end
+
+  end
+
+  def update
+    book = Book.find(params[:id])
+
+    book.update(book_params)
+
+    redirect_to book_path(book.id)
+
+    flash[:notice] = "Book was successfully updated."
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
